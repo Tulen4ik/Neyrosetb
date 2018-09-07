8b4f5829ebe3c0ed888bd722fd769d36629cf742
Tulen4ik <43062592+Tulen4ik@users.noreply.github.com>
2018-09-07 11:58:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();   
+            int ret;
+            int[] array = new int[140];
+            List<int> numbers = new List <int>();
+            for (int i = 0; i < 140; i++)
+            {
+                ret = ran.Next(-100, 100);
+                array[i] = ret;
+                numbers.Add(ret);
+                Console.WriteLine(array[i] + " " + numbers[i]);
+                Console.ReadKey ();
+            }
+            int[,] numbers1 = new int[5,5];
+            numbers1 [1, 2] = 0;
+
+
+        }
+    }
+}
