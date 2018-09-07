5ad521ea33a8444a25bd614fffc61c08852065ad
Tulen4ik <43062592+Tulen4ik@users.noreply.github.com>
2018-09-07 20:32:33 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            WebClient wc = new WebClient();
+            string answer = wc.DownloadString("https://www.leagueofgraphs.com/ru/summoner/ru/Tulen4ik");
+            richTextBox1.AppendText(answer);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            bool k = false;
+            for (int i = 0; i<richTextBox1.Text.Length-name.Length; i++)
+            {
+                string kek = string.Empty;
+                for (int j = 0; j < name.Length; j++)
+                {
+                    kek += richTextBox1.Text[j + i];
+                    if (string.Equals(kek, name) == true)
+                    {
+                        textBox1.Text = "estb";
+                        k = true;
+                        break;
+                    }
+
+                }
+                if (k==true)
+                {
+                    break;
+                }
+            }
+            if (k==false)
+            {
+                textBox1.Text = "net";
+            }
+        }
+    }
+}
